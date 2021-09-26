930ed27f2ce6f931ec8e56e1cc9679e13a0218b6
matheusximeness <matheus.ximenes@ufv.br>
2021-09-26 19:39:08 -0300

    PSP 0.0

--- ProgramaPSP.java
+++ ProgramaPSP.java
@@ -49,35 +49,35 @@ public class ProgramaPSP {
 	public static char solve( int[] distA ,int[] distD, int attackers, int defenders  )
 	{
 		for( int i = 0 ; i < attackers ; i++ ){
-            for( int j = i + 1 ; j < attackers ; j++ ){
-                if ( distA[ i ] > distA[ j ] )
+			for( int j = i + 1 ; j < attackers ; j++ ){
+				if ( distA[ i ] > distA[ j ] )
 				{
-                    int temp = distA[i]; 	
-                    distA[i] = distA[j];
-                    distA[j] = temp;					
+					int temp = distA[i]; 	
+					distA[i] = distA[j];
+					distA[j] = temp;					
 				}
-            }
-        }
-        
-        for( int i = 0 ; i < defenders ; i++ ){
-            for( int j = i + 1 ; j < defenders ; j++ ){
-                if ( distD[ i ] > distD[ j ] )
-				{
-                    int temp = distD[i]; 	
-                    distD[i] = distD[j];
-                    distD[j] = temp;					
-				}
-            }
-        }
+			}
+		}
 		
-        if( distA[0] < distD[1] )
-        {
-            System.out.println("Y");
-            return 'Y';
-        }
-        System.out.println("N");
-        
-        return 'N';	
+		for( int i = 0 ; i < defenders ; i++ ){
+			for( int j = i + 1 ; j < defenders ; j++ ){
+				if ( distD[ i ] > distD[ j ] )
+				{
+					int temp = distD[i]; 	
+					distD[i] = distD[j];
+					distD[j] = temp;					
+				}
+			}
+		}
+		
+		if( distA[0] < distD[1] )
+		{
+			System.out.println("Y");
+			return 'Y';
+		}
+		System.out.println("N");
+		
+		return 'N';	
 	}
 
 }
